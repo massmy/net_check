67ab57175277a0f186e052d4df132cae4d22e25d
Max <massmy@users.noreply.github.com>
2021-08-30 20:25:09 +0200

    added serde & bincode

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
+// use core::panic;
 use std::{env, io, net::SocketAddr, sync::mpsc::{self, Receiver}, thread};
 use std::net::UdpSocket;
+use serde::{Serialize, Deserialize};
 
-#[cfg(debug_assertions)]
-macro_rules! debug {
-    ($x:expr) => { dbg!($x) }
+// #[cfg(debug_assertions)]
+// macro_rules! debug {
+//     ($x:expr) => { dbg!($x) }
+// }
+
+// #[cfg(not(debug_assertions))]
+// macro_rules! debug {
+//     ($x:expr) => { std::convert::identity($x) }
+// }
+
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct Payload{
+    pub data: i32
 }
 
-#[cfg(not(debug_assertions))]
-macro_rules! debug {
-    ($x:expr) => { std::convert::identity($x) }
+impl Payload {
+    fn new(data: i32) -> Self { Self { data } }
 }
 
 fn main() {
@@ -19,15 +31,13 @@ fn main() {
         // let timeout = Duration::new(0, 1000000000);
         loop {
             match stdin_channel.try_recv() {
-                Ok(elem) => {}, //println!("{:?}", elem),
+                Ok(_elem) => {}, //println!("{:?}", elem),
                 Err(_) => sleep(10),
             }
         }
     }else if args.len() > 1 {
         start_sender(&(args[1].as_str().to_owned() + ":34254"));
-        //start_sender("127.0.0.1:34254")
     }
-    // println!("Hello, world!");
 }
 
 fn sleep(millis: u64) {
@@ -36,7 +46,6 @@ fn sleep(millis: u64) {
 }
 
 fn create_udp_socket_receiver(host: &str) -> io::Result<UdpSocket> {
-    // debug!("initializing listener udp socket on {}", host);
     let socket = UdpSocket::bind(&host)?;
     return Ok(socket);
 }
@@ -44,27 +53,34 @@ fn create_udp_socket_receiver(host: &str) -> io::Result<UdpSocket> {
 fn create_udp_socket_sender(host: &str) -> io::Result<UdpSocket> {
     let local_address = "0.0.0.0:0";
     let socket = UdpSocket::bind(local_address)?;
+    // let timeout = Some(std::time::Duration::from_nanos(100000));
+    // socket.set_read_timeout(timeout)?;
+    // socket.set_write_timeout(timeout)?;
     let socket_address: SocketAddr = host
         .parse::<SocketAddr>()
         .expect("Invalid forwarding address specified");
     socket.connect(&socket_address)?;
-    // debug!("initializing listener udp socket on {}", local_address);
     return Ok(socket);
 }
 
 fn start_sender(host: &str) {
     let listen_socket = create_udp_socket_sender(host).expect("failed to bind host socket");
-    for num in 0..1000000 {
-        let buf = u32::to_le_bytes(num);
-        let res = send(&listen_socket, host, &buf);
+    for num in 0..100000 {
+        let payload = Payload::new (num );
+        let encoded: Vec<u8> = bincode::serialize(&payload).unwrap();
+        let res = send(&listen_socket, host, &encoded);
         if let Some(tmp_res) = res{
-            let mut tmp = [0; 4];
-            for i in 0..3 {
-                tmp[i] = tmp_res[i];
-            }
-            let tmp_i = u32::from_le_bytes(tmp);
-            if num != tmp_i{
-                println!("{}, {}", num, tmp_i);
+            let decoded: Option<Payload> = match bincode::deserialize(&tmp_res[..]){
+                Ok(value) => Some(value),
+                Err(_err) => {
+                    Option::None
+                },
+            };
+            // let decoded: Option<Payload> = bincode::deserialize(&tmp_res[..]).unwrap();
+            if let Some(decoded_payload) = decoded{
+                if num != decoded_payload.data{
+                    println!("{}, {}", num, decoded_payload.data);
+                }
             }
         }
     }
@@ -72,15 +88,15 @@ fn start_sender(host: &str) {
 
 fn send(socket: &UdpSocket, receiver: &str, msg: &[u8]) -> Option<Vec<u8>> {
     // println!("sending message: {:?}", msg);
-    let result: usize = 0;
+    // let result: usize = 0;
     match socket.send_to(&msg, receiver) {
-        Ok(number_of_bytes) => {},//println!("{:?}", number_of_bytes),
+        Ok(_number_of_bytes) => {},//println!("{:?}", number_of_bytes),
         Err(fail) => println!("failed sending {:?}", fail),
     }
-    let mut buf = [0; 20];
+    let mut buf = [0; 4];
     return match socket.recv(&mut buf){
-        Ok(number_of_bytes) => Option::Some(Vec::from(&buf[0..number_of_bytes])),
-        Err(_) => Option::None,
+        Ok(number_of_bytes) => Some(Vec::from(&buf[0..number_of_bytes])),
+        Err(_) => None,
     };
 }
 
@@ -88,7 +104,7 @@ fn start_server(host: &str) -> Receiver<Vec<u8>>{
     let (tx, rx) = mpsc::channel::<Vec<u8>>();
     let socket = create_udp_socket_receiver(host).unwrap();
     thread::spawn(move || {
-        let mut buf = [0; 20];
+        let mut buf = [0; 4];
         let mut result: Vec<u8>;
         loop{
             match socket.recv_from(&mut buf){
